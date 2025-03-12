642a739fffa1e0d2564530c0466fefff3fb427fc
Pedro Bonfilio <pedroblimaa@gmail.com>
2025-03-12 19:45:00 -0300

    Add project and timeline animation

--- src/components/TimelineItem/TimelineCard/TimelineCard.tsx
+++ src/components/TimelineItem/TimelineCard/TimelineCard.tsx
@@ -4,16 +4,16 @@ import './TimelineCard.scss'
 function TimelineCard({ position, itemInfo }: TimelineCardProps) {
   return (
     <div className={`timeline-card ${position}`}>
-      <h3 className='company'> {itemInfo.company}</h3>
-      <h2 className='role'>{itemInfo.role}</h2>
-      <ul className='bullets'>
-        {itemInfo.bullets.map((bullet, index) => (
-          <li key={index}>
-            <span className='bullet-title'>{bullet.title}</span> -
-            {bullet.description}
-          </li>
-        ))}
-      </ul>
+        <h3 className='company'> {itemInfo.company}</h3>
+        <h2 className='role'>{itemInfo.role}</h2>
+        <ul className='bullets'>
+          {itemInfo.bullets.map((bullet, index) => (
+            <li key={index}>
+              <span className='bullet-title'>{bullet.title}</span> -
+              {bullet.description}
+            </li>
+          ))}
+        </ul>
     </div>
   )
 }
--- src/components/TimelineItem/TimelineItem.tsx
+++ src/components/TimelineItem/TimelineItem.tsx
@@ -1,32 +1,65 @@
+import { motion } from 'motion/react'
+
 import TimelineCard from './TimelineCard/TimelineCard'
 import './TimelineItem.scss'
 import { Position, TimelineCardProps } from './TimelineTypes'
 
 function TimelineItem({ position, itemInfo }: TimelineCardProps) {
+  const createVariants = (direction: 'left' | 'right') => ({
+    offscreen: {
+      opacity: 0,
+      x: direction === 'left' ? -200 : 200,
+    },
+    onscreen: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        period: 0.3,
+      },
+    },
+  })
+
+  const cardVariants = createVariants(
+    position === Position.Left ? 'left' : 'right'
+  )
+  const durationVariants = createVariants(
+    position === Position.Left ? 'right' : 'left'
+  )
+
+  const renderItem = (type: 'card' | 'period') => {
+    if (type === 'card') {
+      return (
+        <motion.div variants={cardVariants}>
+          <TimelineCard itemInfo={itemInfo} position={position} />
+        </motion.div>
+      )
+    }
+
+    return (
+      <motion.div variants={durationVariants}>
+        <div className={`period-container ${position}`}>
+          <div className='period'>{itemInfo.period}</div>
+        </div>
+      </motion.div>
+    )
+  }
+
   return (
-    <div className='timeline-item'>
-      {position === Position.Left ? (
-        <>
-          <TimelineCard itemInfo={itemInfo} position={position} />
-          <div className='company-card'>
-            <img src={itemInfo.companyImg} alt={itemInfo.company} />
-          </div>
-          <div className={`duration-container ${position}`}>
-            <div className='duration'>{itemInfo.period}</div>
-          </div>
-        </>
-      ) : (
-        <>
-          <div className={`duration-container ${position}`}>
-            <div className='duration'>{itemInfo.period}</div>
-          </div>
-          <div className='company-card'>
-            <img src={itemInfo.companyImg} alt={itemInfo.company} />
-          </div>
-          <TimelineCard itemInfo={itemInfo} position={position} />
-        </>
-      )}
-    </div>
+    <motion.div
+      initial='offscreen'
+      whileInView='onscreen'
+      viewport={{ amount: 0.8 }}
+    >
+      <div className='timeline-item'>
+        {position === Position.Left ? renderItem('card') : renderItem('period')}
+        <div className='company-card'>
+          <img src={itemInfo.companyImg} alt={itemInfo.company} />
+        </div>
+        {position === Position.Right
+          ? renderItem('card')
+          : renderItem('period')}
+      </div>
+    </motion.div>
   )
 }
 
